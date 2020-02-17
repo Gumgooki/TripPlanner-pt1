7a6b34f0df8d8e726e9e6a0300e68eecc76b1b9a
Thomas Bunch <17713462+Gumgooki@users.noreply.github.com>
2020-02-17 09:32:49 -0600

    watched the solution video

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-console.log('this is a test message');
 const mapboxgl = require('mapbox-gl');
-const markers = require('./marker.js');
+const buildMarker = require('./marker.js');
 
 mapboxgl.accessToken =
   'pk.eyJ1IjoiZ3VtZ29va2kiLCJhIjoiY2s2bGs3aGpjMGR6NjNsbzc4MGdjZmNncyJ9.BiYqpTga-cJZL5CymsZxGQ';
+
 const map = new mapboxgl.Map({
   container: 'map',
   center: [-87.6354, 41.8885], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
@@ -18,7 +18,6 @@ markerDomEl.style.backgroundImage = 'url(http://i.imgur.com/WbMOfMl.png)';
 
 new mapboxgl.Marker(markerDomEl).setLngLat([-87.641, 41.895]).addTo(map); // [] for Chicago
 
-const testCoords = [-87.641, 41.895];
-markers(testCoords, 'Activity');
-
-module.exports = map;
+const testCoords = [-87.641, 41.85];
+const marker = buildMarker('hotels', testCoords);
+marker.addTo(map);
--- src/marker.js
+++ src/marker.js
@@ -1,19 +1,36 @@
-const mapboxgl = require('mapbox-gl');
-const map = require('./index.js');
+const { Marker } = require('mapbox-gl');
 
-module.exports = function markerFactory(coords, type) {
-  let image = '';
-  if (type === 'Restaurant') {
-    image = 'http://i.imgur.com/cqR6pUI.png';
-  } else if (type === 'Hotel') {
-    image = 'http://i.imgur.com/D9574Cu.png';
-  } else if (type === 'Activity') {
-    image = 'http://i.imgur.com/WbMOfMl.png';
-  }
-  const touristMarker = document.createElement('div');
-  touristMarker.style.width = '32px';
-  touristMarker.style.height = '39px';
-  touristMarker.style.backgroundImage = `url(${image})`;
-
-  new mapboxgl.Marker(touristMarker).setLngLat(coords).addTo(map); // [] for Chicago
+const iconURLs = {
+  hotels: 'https://i.imgur.com/D9574Cu.png',
+  restaurants: 'https://i.imgur.com/cqR6pUI.png',
+  activities: 'https://i.imgur.com/WbMOfMl.png',
 };
+
+const buildMarker = (type, coords) => {
+  const markerElement = document.createElement('div');
+  markerElement.style.width = '32px';
+  markerElement.style.height = '37px';
+  markerElement.style.backgroundImage = `url(${iconURLs[type]})`;
+  return new Marker(markerElement).setLngLat(coords);
+};
+
+module.exports = buildMarker;
+
+// const mapboxgl = require('mapbox-gl');
+
+// module.exports = function markerFactory(coords, type) {
+//   let image = '';
+//   if (type === 'restaurants') {
+//     image = 'https://i.imgur.com/cqR6pUI.png';
+//   } else if (type === 'hotels') {
+//     image = 'https://i.imgur.com/D9574Cu.png';
+//   } else if (type === 'activities') {
+//     image = 'https://i.imgur.com/WbMOfMl.png';
+//   }
+//   const touristMarker = document.createElement('div');
+//   touristMarker.style.width = '32px';
+//   touristMarker.style.height = '39px';
+//   touristMarker.style.backgroundImage = `url(${image})`;
+
+//   return new mapboxgl.Marker(touristMarker).setLngLat(coords); // [] for Chicago
+// };
